4a606ff57efa28bc627cc4a9cab4539e48fe6ed3
Melinda Kaszoni <kaszonim@uni.coventry.ac.uk>
2016-10-10 06:32:00 -0400

    finished lab 2 exercise 1.1

--- 02 Objects and Functions/coffee/coffee.js
+++ 02 Objects and Functions/coffee/coffee.js
@@ -2,21 +2,41 @@
 'use strict'
 
 const defaultSize = 8
+const defaultShots = 0
 
-module.exports = function(roast, ounces = defaultSize) { // can't use arrow functions here...
+module.exports = function(roast, ounces = defaultSize, shots = defaultShots) { // can't use arrow functions here...
 	if (roast === undefined) {
 		throw new Error('missing roast type')
 	}
 	this.roast = roast
 	this.ounces = ounces
+	this.shots = shots
 	this.getSize = () => {
-		if (this.ounces === 8) {
+		//Switch statement
+		/*switch (this.ounces){
+			case 8: return 'small';
+			case 12: return 'medium';
+			case 16: return 'large';
+			default: break;				
+		}*/
+		
+		//Modified if statement
+		if (this.ounces <= 8){
+			return 'Small'
+		}else if (this.ounces >= 9 && this.ounces <= 12){
+			return 'Medium'
+		}else if (this.ounces > 12){
+			return 'Large'
+		}
+		
+		//Original if statement
+		/*if (this.ounces === 8) {
 			return 'small'
 		} else if (this.ounces === 12) {
 			return 'medium'
 		} else if (this.ounces === 16) {
 			return 'large'
-		}
+		}*/
 	}
 	this.order = () => {
 		let msg
@@ -24,7 +44,11 @@ module.exports = function(roast, ounces = defaultSize) { // can't use arrow func
 		case 'small':
 		case 'medium':
 		case 'large':
-			msg = `You've ordered a ${this.getSize()} ${this.roast} coffee.`
+				switch (this.shots){
+					case (this.shots > 2): msg  =`You've ordered a strong ${this.getSize()} ${this.roast} coffee.` 
+						break
+					default: msg = `You've ordered a ${this.getSize()} ${this.roast} coffee.`
+				}
 			break
 		default:
 			msg = `We don't have a ${this.roast} in that size!`
