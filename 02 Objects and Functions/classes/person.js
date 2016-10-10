d6f41662d8fe513fc3cf64bd6ba0885c31dc6858
Melinda Kaszoni <kaszonim@uni.coventry.ac.uk>
2016-10-10 07:44:37 -0400

    in progress 3.1.1

--- 02 Objects and Functions/classes/person.js
+++ 02 Objects and Functions/classes/person.js
@@ -7,13 +7,15 @@ module.exports = class Person {
 	 * Create a person
 	 * @param {string} firstname - the person's first name
 	 * @param {string} lastname - the person's last name
+	 * @param {Date()} dob - the person's date of birth
 	 */
-	constructor(firstname, lastname) {
-		if (firstname === undefined || lastname === undefined) {
+	constructor(firstname, lastname, dob) {
+		if (firstname === undefined || lastname === undefined || dob === undefined) {
 			throw new Error('missing parameter')
 		}
 		this.first = firstname
 		this.last = lastname
+		this.dob = new Date(dob)
 	}
 
 	/**
@@ -31,13 +33,28 @@ module.exports = class Person {
 	set lastName(name) {
 		this.last = name
 	}
+	
+	/**
+	 * Set the person's date of birth
+	 * @param {Date()} name - the person's last name
+	 */
+	set dob(dob){
+		this.dob = new Date(dob)
+	}
 
 	/**
-	 * Set the person's first name
+	 * Get the person's full name
 	 * @return {string} the person's full name
 	 */
 	get name() {
 		return `${this.first} ${this.last}`
 	}
-
+	
+	/**
+	 * Get the person's date of birth
+	 * @return {Date()} the person's age (in years)
+	 */
+	get dob(){
+		return Date.now().getFullYear() - this.dob.getFullYear();
+	}
 }
--- 02 Objects and Functions/classes/personTest.js
+++ 02 Objects and Functions/classes/personTest.js
@@ -2,12 +2,14 @@
 
 const Person = require('./person')
 try {
-	const person = new Person('Andy', 'Capp')
+	var dateOfBirth = new Date('February 3, 2000');
+	const person = new Person('Andy', 'Capp', dateOfBirth)
 	console.log(person.name)
 	person.lastName = 'Pandy'
 	console.log(JSON.stringify(person, null, 2))
+	console.log(Date.now().getFullYear() - dateOfBirth.getFullYear());
 
-	const badPerson = new Person('anon')
+	//const badPerson = new Person('anon')
 } catch(err) {
 	console.log(`ERROR: ${err}`)
 }
