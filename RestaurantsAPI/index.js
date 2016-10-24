c4e65abc1a47c40ede12552cc574f3b4bc164232
Melinda Kaszoni <kaszonim@uni.coventry.ac.uk>
2016-10-24 06:38:57 -0400

    working call

--- RestaurantsAPI/index.js
+++ RestaurantsAPI/index.js
@@ -1,46 +1,15 @@
 'use strict'
 
-//const data = require('./restaurants')
+const rest = require('./restaurants')
 const request = require('request')
 const https = require('https')
 
 const categories = 'categories'
-apiCall(categories)
-
-function apiCall(name) {
-	const host = `https://developers.zomato.com`  
-  const endpoint = `/api/v2.1/${name}`
-  const apiKey = '53d2f755e44b12d31be6f3db16d397c9'
-	console.log(host + endpoint)
-  
-  var options = {
-    host: host,
-    path: endpoint,
-    method: 'GET',
-    headers: {
-      'accept': 'application/json'
-      }
-  };
-
-  https.request(options, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-    
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      console.log('BODY: ' + chunk);
-    });
-  }).end({
-    'user-key': '53d2f755e44b12d31be6f3db16d397c9'
-  });
-}
-  
-  
-/*data.getData(categories, (err, res) => {
-  try {
+rest.getData(categories,(err, res) => {
+	try {
 		if (err) throw err
-		console.log(JSON.stringify(res, null, 2))
+		console.log(res)
 	} catch(err) {
 		console.log(`ERROR: ${err.message}`)
 	}
-})*/
+})
--- RestaurantsAPI/restaurants.js
+++ RestaurantsAPI/restaurants.js
@@ -3,7 +3,6 @@
 const request = require('request')
 const https = require('https')
 
-
 exports.getData = (name, callback) => {
   apiCall(name, (err, res) => {
     if (err) return callback(err)
@@ -11,33 +10,34 @@ exports.getData = (name, callback) => {
   })
 }
 
-function apiCall(name, callback) {
+function apiCall(name) {
 	const host = `developers.zomato.com`  
   const endpoint = `/api/v2.1/${name}`
-  const apiKey = '53d2f755e44b12d31be6f3db16d397c9'
 	console.log(host + endpoint)
   
   var options = {
-  host: host,
-  path: endpoint,
-  method: 'GET',
-  haders: {
-    'user-key': apiKey
-  }
-};
-
-https.request(options, function(res) {
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
-  res.setEncoding('utf8');
-  res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
-  });
-}).end();
-	
-  //quest.get(url, (err, res, body) => {
-  // (err) return callback(new Error('API error'))
-  //nst json = JSON.parse(body)
-  //turn callback(null, json)
-//
+    host: host,
+    path: endpoint,
+    method: 'GET',
+		port: 443,
+    headers: {
+			'user-key': '53d2f755e44b12d31be6f3db16d397c9',
+      'accept': 'application/json',
+			'content-type': 'application/json'
+      }
+  };
+		
+	https.request(options, function(res) {
+    //console.log('STATUS: ' + res.statusCode)
+    //console.log('HEADERS: ' + JSON.stringify(res.headers));
+		
+		res.setEncoding('utf8');
+    res.on('data', function (chunk) {	
+			var data = JSON.parse(chunk)
+			for(var c in data.categories){
+				console.log(data.categories[c].categories.id + '. ' + data.categories[c].categories.name)
+			}
+			//chunk = JSON.parse(chunk)
+    });
+  }).end();
 }
