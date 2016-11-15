f250b084a81f9523cf9bb949446765ad68c9654c
Melinda Kaszoni <kaszonim@uni.coventry.ac.uk>
2016-11-15 18:27:17 +0000

    clean folders

--- API/test.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-let restDetails = {'items_sown':'29','restaurants': [{'name':'test','location':'test'}]}
-//console.log(restDetails)
-restDetails.restaurants.push({'name':'test2', 'location': 'test2'})
-console.log(restDetails)
