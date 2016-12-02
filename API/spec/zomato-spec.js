41bb9caf2f83cc49aacc9a68ae4eb9b8f086800d
Melinda Kaszoni <kaszonim@uni.coventry.ac.uk>
2016-12-02 22:49:27 +0000

    zomato testing reorganised

--- API/spec/zomato-spec.js
+++ API/spec/zomato-spec.js
@@ -2,94 +2,99 @@
 
 var data = require("../modules/zomato")
 
-describe('Restaurant API', function () {
-  it('getCategories - results returned', done => {
-    data.getCategories().then( (response) => {
-		  expect(response.length).toBeGreaterThan(0)
-      done()
-    })
-	})
+describe('Restaurant API', () => {
+	describe('getCategories', () => {
+		it('should return results', done => {
+			data.getCategories().then( (response) => {
+				expect(response.length).toBeGreaterThan(0)
+				done()
+			})
+		})
 
-	it('getCategories - results not found', done => {
-		data.getCategories().then( response => {
-			expect(response).toBe('')
-			done()
-		}, (err) => {
-			expect(err.message).toBe('No categories found')
-			done()
-		}).catch ( err => {
-			expect(err.message).toBe('No categories found')
-			done() 
-		})
-	})
-
-	it('getLocationDetails - results', done => {
-		data.getLocationDetails('coventry').then( response => {
-			expect(response.id).toBe(94264)
-			expect(response.type).toBe('zone')
-			done()
-		})
-	})
-	
-	it('getLocationDetails - no location details found', done => {
-		data.getLocationDetails('query').catch( err => {
-			expect(err.message).toBe('No location details found')
-			done()
-		})
-	})
-	
-	it('getRestaurants - return message', done => {
-		data.getRestaurants(94264, 'zone').then( result => {
-			expect(result[0].items_found).toBe(179)
-			done()
-		})
-	})
-	
-	it('getRestaurants - no restaurants found for wrong type', done => {
-		data.getRestaurants(94264, 'city').then( result => {
-			expect(result).toBe('')
-			done()
-		}, (err) => {
-			expect(err).toBe('No restaurants found')
-			done()
-		})
-	})
-	
-	it('getRestaurants - no restaurants found for wrong id', done => {
-		data.getRestaurants(1251, 'zone').then( result => {
-			expect(result).toBe('')
-			done()
-		}, (err) => {
-			expect(err).toBe('No restaurants found')
-			done()
-		})
-	})
-	
-	it('getRestaurants - response body', done => {
-		data.getRestaurants(94264, 'zone').then( result => {
-			expect(result[0].items_shown).toBe(result[0].items_found)
-			done()
+		it('should error if no results found', done => {
+			data.getCategories().then( response => {
+				expect(true).toBe(false)
+				done()
+			}).catch ( err => {
+				expect(err.message).toBe('No categories found')
+				done() 
+			})
 		})
 	})
 
-	it('getRestaurantsById - response', done => {
-		data.getRestaurantsById(16681615).then( result => {
-			expect(result.name).toBe('Cosmos')
-			done()
+	describe('getLocationDetails', () => {
+		it('should find results', done => {
+			data.getLocationDetails('coventry').then( response => {
+				expect(response.id).toBe(94264)
+				expect(response.type).toBe('zone')
+				done()
+			})
+		})
+		
+		it('should not find any location details', done => {
+			data.getLocationDetails('query').catch( err => {
+				expect(err.message).toBe('No location details found')
+				done()
+			})
 		})
 	})
 
-	it('getRestaurantsById - invalid id', done => {
-		data.getRestaurantsById().catch( err => {
-			expect(err.message).toBe('Invalid restaurant ID')
-			done()
+	describe('getRestaurants', () => {
+		it('shoudl return restaurants', done => {
+			data.getRestaurants(94264, 'zone').then( result => {
+				expect(result[0].items_found).toBe(179)
+				done()
+			})
+		})
+		
+		it('should not find restaurants for wrong type', done => {
+			data.getRestaurants(94264, 'city').then( result => {
+				expect(true).toBe(false)
+				done()
+			}, (err) => {
+				expect(err).toBe('No restaurants found')
+				done()
+			})
+		})
+		
+		it('should not find restaurants for wrong id', done => {
+			data.getRestaurants(1251, 'zone').then( result => {
+				expect(result).toBe('')
+				done()
+			}, (err) => {
+				expect(err).toBe('No restaurants found')
+				done()
+			})
+		})
+		
+		it('should return full response body', done => {
+			data.getRestaurants(94264, 'zone').then( result => {
+				expect(result[0].items_shown).toBe(result[0].items_found)
+				done()
+			})
 		})
 	})
 
-	it('getRestaurantsById - no results', done => {
-		data.getRestaurantsById(10000000).catch( err => {
-			expect(err.message).toBe('Restaurant with ID 10000000 cannot be found')
-			done()
+	describe('getRestaurantsById', () => {
+		it('should return restaurant', done => {
+			data.getRestaurantsById(16681615).then( result => {
+				expect(result.name).toBe('Cosmos')
+				done()
+			})
+		})
+
+		it('should not return for invalid id', done => {
+			data.getRestaurantsById().catch( err => {
+				expect(err.message).toBe('Invalid restaurant ID')
+				done()
+			})
+		})
+
+		it('should return no results', done => {
+			data.getRestaurantsById(10000000).catch( err => {
+				expect(err.message).toBe('Restaurant with ID 10000000 cannot be found')
+				done()
+			})
 		})
 	})
 })
