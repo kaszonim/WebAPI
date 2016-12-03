450dd5ede10151cca19d007a12c4642897df092f
Melinda Kaszoni <kaszonim@uni.coventry.ac.uk>
2016-12-03 12:27:18 +0000

    reorganised schema testing

--- API/spec/persistence-spec.js
+++ API/spec/persistence-spec.js
@@ -2,72 +2,88 @@
 
 var persist = require("../modules/persistence")
 
-describe('API data persistence', function () {
+describe('API data persistence', () => {
 
-    it('createUser - invalid user object', (done) => {
-        const details = {
-            'password': '',
-            'name': 'test name'
-        }
+    describe('createUser', () => {
+        it('should fail - invalid user object', done => {
+            const details = {
+                'password': '',
+                'name': 'test name'
+            }
 
-        persist.createUser(details).then( (response) => {
-            expect(response).toBe(null)
-            done()
-        }).catch( err => {
-            expect(err.message).toBe('invalid user object')
-            done()
+            persist.createUser(details).then( response => {
+                if (response) expect(true).toBe(false)
+                done()
+            }).catch( err => {
+                expect(err.message).toBe('invalid user object')
+                done()
+            })
         })
-    })
 
-    it('createUser - success', (done) => {
-        const details = {
-            'username': 'testuser',
-            'password': 't3stus3r',
-            'name': 'Test User'
-        }
+        it('should create user', done => {
+            const details = {
+                'username': 'testuser',
+                'password': 't3stus3r',
+                'name': 'Test User'
+            }
 
-        persist.createUser(details).then( (response) => {
-            expect(response.username).toBe('testuser')
-            expect(response.password).toBe(undefined)
-            expect(response.name).toBe('Test User')
-            done()
+            persist.createUser(details).then( response => {
+                expect(response.username).toBe('testuser')
+                expect(response.password).toBe(undefined)
+                expect(response.name).toBe('Test User')
+                done()
+            })
         })
-    })
 
-    xit('createUser - failure', (done) => {
-        const details = {
-            'username': 'testuser',
-            'password': 't3stus3r',
-            'name': 'Test User'
-        }
+        it('should fail', done => {
+            const details = {
+                'username': 'testuser',
+                'password': 't3stus3r',
+                'name': 'Test User'
+            }
 
-        persist.createUser(details).then( (response) => {
-            expect(response).toBe(null)
-            done()
-        }).catch( err => {
-            expect(err.message).toBe('error creating account')
-            done()
+            persist.createUser(details).then( response => {
+                if (response) expect(true).toBe(false)
+                done()
+            }).catch( err => {
+                expect(err.message).toBe('error creating account')
+                done()
+            })
         })
-    })
+     })
 
-    it('getUsers - error', (done) => {
-        persist.getUsers().then( (response) => {
-            expect(response).toBe(null)
-            done()
-        }).catch( err => {
-            //console.log(err)
-            //expect(err.message).toBe('error getting ussers')
-            done()
+     describe('getUsers', () => {
+        it('should return users', done => {
+            persist.getUsers().then( response => {
+                expect(response.length).toBe(2)
+                done()
+            }).catch( err => {
+                expect(true).toBe(false)
+                done()
+            })
         })
-    })
+            
+        it('should fail', done => {
+            persist.getUsers().then( response => {
+                if (response) expect(true).toBe(false)
+                done()
+            }).catch( err => {
+                //console.log(err)
+                expect(err.message).toBe('no users found')
+                done()
+            })
+        })
+     })
 
-    it('deleteUsers - success', (done) => {
-        persist.deleteUsers().then( (response) => {
-            expect(response).toBe('All users removed successfully')
-            done()
-        }).catch( err => {
-            expect(err).toBe(null)
-            done()
+     describe('deleteUsers', () => {
+        it('should delete all users', done => {
+            persist.deleteUsers().then( response => {
+                expect(response).toBe('All users removed successfully')
+                done()
+            }).catch( err => {
+                if (err) expect(true).toBe(false)
+                done()
+            })
         })
-    })
+     })
 })
