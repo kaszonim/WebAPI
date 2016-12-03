1bff49e9bdbed224c5e5e7b0d9acd12d0921f8a6
Melinda Kaszoni <kaszonim@uni.coventry.ac.uk>
2016-12-03 13:31:27 +0000

    fixed linting errors

--- API/modules/persistence.js
+++ API/modules/persistence.js
@@ -6,58 +6,36 @@ exports.createUser = details => new Promise( (resolve, reject) => {
 	if(!details.username || !details.password || !details.name){
 		reject(new Error('invalid user object'))
 	} else {
-		const user = new schema.User(details)
-
-		user.save( (err, user) => {
+		new schema.User(details).save( (err, user) => {
 			if (err) {
 				reject(new Error('error creating account'))
 			}
-			delete details.password
-			resolve(details)
+			delete user.password
+			resolve(user)
 		})
 	}
 })
 
-exports.getUsers = () =>  {
-	return new Promise( (resolve, reject) => {
-		const result = ''
-		schema.User.find({}, function(err, users) {
-			if (err) {
-				reject(err)
-			}
-			// object of all the users
-			if (users.length === 0) {
-				reject(new Error('no users found'))
-			}
-			resolve(users)
-		})
+exports.getUsers = () => new Promise( (resolve, reject) => {
+	schema.User.find({}, function(err, users) {
+		if (err) reject(err)
+		if (users.length === 0) reject(new Error('no users found'))
+		resolve(users)
 	})
-}
+})
 
-exports.deleteUsers = inUsername => {
-	return new Promise( (resolve, reject) => {
-		if(inUsername === undefined){
-			schema.User.remove(function(err, removed) {
-				if (err) {
-					reject(err)
-				}
-				if (removed === 0){
-					reject(new Error('users cannot be deleted'))
-				} else {
-					resolve('All users removed successfully')
-				}
-			})
-		} else {
-			schema.User.remove({ username: inUsername}, function(err, removed) {
-				if (err) {
-					reject(err)
-				}
-				if (removed === 0){
-					reject(new Error('users cannot be deleted'))
-				} else {
-					resolve(`${inUsername} deleted successfully`)
-				}
-			})
-		}
-	})
-}
+exports.deleteUsers = inUsername => new Promise( (resolve, reject) => {
+	if(inUsername === undefined){
+		schema.User.remove(function(err, removed) {
+			if (err) reject(err)
+			if (removed === 0) reject(new Error('users cannot be deleted'))
+			resolve('All users removed successfully')
+		})
+	} else {
+		schema.User.remove({ username: inUsername}, function(err, removed) {
+			if (err) reject(err)
+			if (removed === 0) reject(new Error('users cannot be deleted'))
+			resolve(`${inUsername} deleted successfully`)
+		})
+	}
+})
